2da3ac25ab907e5c29dc9ba2452a519c41f8a97b
ymgyt <yamaguchi7073xtt@gmail.com>
2019-04-30 20:12:28 +0900

    add golangci-lint to deps tools

--- tools.go
+++ tools.go
@@ -4,6 +4,7 @@ package tools
 
 import (
 	_ "github.com/CircleCI-Public/circleci-cli"
+	_ "github.com/golangci/golangci-lint/cmd/golangci-lint"
 	_ "github.com/google/wire/cmd/wire"
 	_ "github.com/magefile/mage"
 )
